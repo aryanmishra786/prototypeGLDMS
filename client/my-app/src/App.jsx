37a58531b2a255e86f364a5ea7bbd937e2f7bd20
aryanmishra786 <mishra.aryan10@gmail.com>
2025-06-29 02:34:41 -0700

    react/frontend set up

---
+++ client/my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div>
+      <h1>welcome to the app</h1>
+    </div>
+  )
+}
+
+export default App
